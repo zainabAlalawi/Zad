295c6587631eed8c67b305024a74db774535635b
zainabAlalawi <39020174+zainabAlalawi@users.noreply.github.com>
2018-12-15 20:37:44 +0400

    Initial commit

---
+++ app/src/main/java/herfa/sas/com/zad/MainActivity.java
@@ -0,0 +1,13 @@
+package herfa.sas.com.zad;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
